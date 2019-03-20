fe16579aa31696a64642dd31abf291880966e51a
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2019-03-20 14:59:25 -0300

    Add graphql-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import config from 'config'
+import resprofile from 'queries/restaurant-profile.graphql'
 
 const HelloMessage = () => <h1>Hola mundo</h1>
+
+console.log(resprofile)
 
 ReactDOM.render(
   <HelloMessage />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const exclude = /node_modules/
+
 module.exports = {
   entry: './src/client/index.js',
   output: {
@@ -8,7 +10,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude, loader: "babel-loader" },
+      { test: /\.(graphql|gql)$/, exclude, loader: 'graphql-tag/loader' }
     ]
   },
   resolve: {
