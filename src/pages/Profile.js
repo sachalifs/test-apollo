c51aed66118ccf1b9a69eee9b151151492812f91
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2019-03-21 11:06:50 -0300

    Add more fields to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,6 +5,9 @@ import { graphql } from 'react-apollo'
 import resprofile from 'src/queries/restaurant-profile.graphql'
 import Header from 'src/components/Header'
 import PhotosCarousel from 'src/components/PhotosCarousel'
+import FormatNumber from 'format-number'
+
+const format = FormatNumber({ integerSeparator: '.' })
 
 const mapResultsToProps = props => ({
   restaurant: props.data.restaurant,
@@ -33,7 +36,10 @@ class Profile extends Component {
 
     const {
       name,
-      photos
+      photos,
+      numericRating,
+      primaryCuisine,
+      reviews
     } = restaurant
 
     return (
@@ -44,8 +50,28 @@ class Profile extends Component {
           </div>
         </div>
         <div className='container mt-3'>
-          <div className='col-xs'>
-            <h1 className='h3'>{restaurant.name}</h1>
+          <div className='row'>
+            <div className='col'>
+              <h1 className='h2'>{name}</h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-9 h5'>{primaryCuisine.name}</div>
+            <div className='col-3'>
+              <span className='h4'>{numericRating}</span>
+              <span className='text-muted'>/10</span>
+            </div>
+          </div>
+          <div className='row text-muted small'>
+            <div className='col-7'>Precio promedio $800</div>
+            <div className='col-5 text-right'>
+              {format(reviews.count)} opiniones
+            </div>
+          </div>
+          <div className='row mt-3'>
+            <div className='col'>
+              <button type='button' className='btn btn-primary btn-block'>Book a table</button>
+            </div>
           </div>
         </div>
       </div>
