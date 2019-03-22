9f82a64647008d3595597e7132fdedd9852ad937
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2019-03-22 12:07:40 -0300

    Sanitize cuisine. Use functional component

--- src/components/CuisineSelector/index.js
+++ src/components/CuisineSelector/index.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types'
 import './styles.scss'
 
 const capitalize = str => str[0].toUpperCase() + str.slice(1).toLowerCase()
+const sanitize = str => str.replace(/-/gi, ' ')
 
-class CuisineSelector extends React.Component {
-  render() {
-    const { cuisines, selectedCuisine, onSelectedCuisineChange } = this.props
-
-
-    console.log({ cuisines })
-    return (
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Tipo de cocina: {capitalize(selectedCuisine)}
+const CuisineSelector = ({ cuisines, selectedCuisine, onSelectedCuisineChange }) => (
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Tipo de cocina: {sanitize(capitalize(selectedCuisine))}
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+      {cuisines.map((cuisine, i) =>
+        <button class="dropdown-item" type="button" key={i} onClick={() => onSelectedCuisineChange(cuisine.slug)}>
+          {cuisine.name}
         </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          {cuisines.map((cuisine, i) =>
-            <button class="dropdown-item" type="button" key={i} onClick={() => onSelectedCuisineChange(cuisine.slug)}>
-              {cuisine.name}
-            </button>
-          )}
-        </div>
-      </div>
-    )
-  }
-}
+      )}
+    </div>
+  </div>
+)
 
 CuisineSelector.propTypes = {
   cuisines: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
